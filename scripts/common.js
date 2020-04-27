b851d4b2a09ab3052a79c0fabaddd133efb39651
SergeyHaritonchik <Siarhey.Kharitonchik@wolterskluwer.com>
2020-04-27 13:18:12 +0300

    add common menu set

---
+++ scripts/common.js
@@ -0,0 +1,19 @@
+class Common {
+    constructor(wrapper) {
+        this.wrapper = wrapper
+    }
+
+    drawMenu() {
+        this.wrapper.insertAdjacentHTML('afterbegin', `   
+         <header>
+                <nav>
+                    <ul>
+                        <li><a href="companies.html">Company</a></li>
+                        <li><a href="monsters.html">Monsters</a></li>
+                        <li><a href="companies.html">Overlord</a></li>
+                        <li><a href="companies.html">Heroes</a></li>
+                    </ul>
+                </nav>
+         </header>`)
+    }
+}
