4e82ed0a72ed204b9719efece3691c29275e5e48
SergeyHaritonchik <Siarhey.Kharitonchik@wolterskluwer.com>
2020-04-28 20:31:40 +0300

    add splig as lieutenant

--- scripts/common.js
+++ scripts/common.js
@@ -10,6 +10,7 @@ class Common {
                     <ul>
                         <li><a href="companies.html">Company</a></li>
                         <li><a href="monsters.html">Monsters</a></li>
+                        <li><a href="lieutenants.html">Lieutenants</a></li>
                         <li><a href="companies.html">Overlord</a></li>
                         <li><a href="companies.html">Heroes</a></li>
                     </ul>
